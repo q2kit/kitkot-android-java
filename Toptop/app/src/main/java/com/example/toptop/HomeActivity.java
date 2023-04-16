3219befe1551f555bba86dd36015ee9350c55110
Lenea28 <ntlena99@gmail.com>
2023-04-16 13:32:44 +0700

    descriptive message

--- Toptop/app/src/main/java/com/example/toptop/HomeActivity.java
+++ Toptop/app/src/main/java/com/example/toptop/HomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CommentActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comment);
+        setContentView(R.layout.activity_home);
     }
 }

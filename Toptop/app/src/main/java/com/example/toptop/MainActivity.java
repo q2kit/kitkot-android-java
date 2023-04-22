6d9537d64826abd0fd0827b5f4a071b83a89710a
Lenea28 <ntlena99@gmail.com>
2023-04-22 21:54:04 +0700

    layout

--- Toptop/app/src/main/java/com/example/toptop/MainActivity.java
+++ Toptop/app/src/main/java/com/example/toptop/MainActivity.java
@@ -30,7 +30,7 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import androidx.core.splashscreen.SplashScreen;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
--- Toptop/app/src/main/java/com/example/toptop/ui/discover/DiscoverFragment.java
+++ Toptop/app/src/main/java/com/example/toptop/ui/discover/DiscoverFragment.java
@@ -61,6 +61,6 @@ public class DiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return inflater.inflate(R.layout.fragment_discovery, container, false);
     }
 }

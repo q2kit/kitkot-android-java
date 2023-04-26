08f9575cda277badf9b20cd2d56ee18dc248aa88
Nguyễn Minh Nghĩa <nghiabadao@May-tinh-xach-tay-cua-Nguyen.local>
2023-04-26 16:52:52 +0700

    feat: like

---
+++ Toptop/app/src/main/java/com/example/toptop/notification/NotificationAdapter.java
@@ -0,0 +1,21 @@
+package com.example.toptop.notification;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotificationAdapter {
+
+    class ViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
